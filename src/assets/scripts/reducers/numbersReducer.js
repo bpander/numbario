615bd289f97a1982790ad1ce9fee4b305f001d65
Brad Anderson <banderso@nerdery.com>
2017-08-02 21:11:21 -0500

    Adds numbers reducer.

---
+++ src/assets/scripts/reducers/numbersReducer.js
@@ -0,0 +1,10 @@
+
+const initialState = {
+  inventory: [],
+  stream: [],
+  target: null,
+};
+
+export default function numbersReducer(state = initialState, action = {}) {
+  return state;
+}
--- src/assets/scripts/reducers/reducer.js
+++ src/assets/scripts/reducers/reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import numbersReducer from 'reducers/numbersReducer';
 import userReducer from 'reducers/userReducer';
 
 
 export default combineReducers({
+  numbers: numbersReducer,
   user: userReducer,
 });
