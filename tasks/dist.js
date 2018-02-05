18bcd959eef73e0011afeeeee3d985ed1ca16a60
Brad Anderson <banderso@nerdery.com>
2018-02-04 20:06:42 -0600

    Updates favicon to satisfy A2HS requirements.

--- tasks/dist.js
+++ tasks/dist.js
@@ -8,7 +8,7 @@ import gulp from 'gulp';
 
 export default function dist() {
   return gulp.src([
-      `${process.env.DIRECTORY_SRC}/favicon.ico`,
+      `${process.env.DIRECTORY_SRC}/favicon.png`,
       `${process.env.DIRECTORY_SRC}/manifest.json`,
     ])
     .pipe(gulp.dest(process.env.DIRECTORY_DEST));
